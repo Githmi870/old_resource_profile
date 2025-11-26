090f7052626f785b002d25551fe06386020f48a8
sasindubm <msasindu450@gmail.com>
2025-11-26 10:04:42 +0530

    docker setup

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start PHP-FPM in background
+php-fpm -D
+
+# Run migrations (optional)
+php artisan migrate --force
+
+# Cache config
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Start Nginx in foreground
+nginx -g "daemon off;"
